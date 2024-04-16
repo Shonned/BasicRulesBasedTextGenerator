7fab37fcbc5e1495d7939b92b330f2dbfdd2290c
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-16 10:13:19 +0100

    comments

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 import spacy
 
+# Load the model
 nlp = spacy.load("en_core_web_sm")
 
 def main():
@@ -8,7 +9,10 @@ def main():
     age = None
     while age is None:
         age_input = input("How old are you? ")
+
+        # Get the age
         age = extract_age(age_input)
+
         if age is not None and age < 18:
             print("You must be over 18 to be eligible for a loan.")
             return
@@ -18,15 +22,28 @@ def main():
         hours_per_week_input = input("How many hours do you work a week? ")
         hours_per_week = extract_hours(hours_per_week_input)
         if hours_per_week is not None and hours_per_week <= 18:
+
+            # Advises the user
             print("If you change your hours from " + str(hours_per_week) + " to 18 your loan would approved.")
+
             return
 
     print("Congratulations! Your loan has been accepted.")
 
 
 def extract_age(input_text):
+    """
+    Structured representation of the text analysed
+    Text: "I am 20"
+    Tokens:
+    1. "I" - Part-of-speech (POS): Personal pronoun
+    2. "am" - POS: Verb
+    3. "20" - POS: Numeral
+    """
     doc = nlp(input_text)
+
     for token in doc:
+        # If token like a number
         if token.like_num:
             return int(token.text)
     print("I couldn't understand your age. Please enter a valid number.")
